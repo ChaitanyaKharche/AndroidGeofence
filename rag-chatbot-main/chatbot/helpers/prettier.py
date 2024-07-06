38cf3946efc353883f30f56f7bc0eaa0399f3c3b
Chaitanya Kharche <kchaitanya1331@gmail.com>
2024-07-06 15:38:04 -0400

    Delete rag-chatbot-main directory

--- rag-chatbot-main/chatbot/helpers/prettier.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def prettify_source(source):
-    document = os.path.basename(source.get("document"))
-    score = source.get("score")
-    content_preview = source.get("content_preview")
-    return f"• **{document}** with score ({round(score,2)}) \n\n **Preview:** \n {content_preview} \n"
